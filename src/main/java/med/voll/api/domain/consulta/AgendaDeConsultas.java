3ac045f5db3211feb8caf5f626af5b59f7d47d69
Alexandre Oliveira <alexmdo@gmail.com>
2023-05-15 21:38:47 -0300

    first commit

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,32 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AgendaDeConsultas {
+
+    private final ConsultaRepository consultaRepository;
+    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
+
+    public void agendar(DadosAgendamentoConsulta dadosAgendamentoConsulta) {
+        var paciente = pacienteRepository.findById(dadosAgendamentoConsulta.idPaciente()).orElseThrow(() -> new RuntimeException("Paciente não encontrado"));
+        var medico = medicoRepository.findById(dadosAgendamentoConsulta.idMedico()).orElseThrow(() -> new RuntimeException("Médico não encontrado"));
+
+        var medico1 = chooseRandomDoctor(dadosAgendamentoConsulta);
+
+        var consulta = new Consulta(null, medico, paciente, dadosAgendamentoConsulta.data());
+        consultaRepository.save(consulta);
+    }
+
+    private Medico chooseRandomDoctor(DadosAgendamentoConsulta dadosAgendamentoConsulta) {
+        return null;
+    }
+
+}
